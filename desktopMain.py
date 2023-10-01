95bff0b7362dcb0289acfae3ac83f93cb0958f40
Ryan-Ellison <Ryan.Ellison477@gmail.com>
2023-10-01 15:21:51 -0400

    First prototype for user inputs

--- desktopMain.py
+++ desktopMain.py
@@ -2,7 +2,7 @@ import sys
 import typing
 from PyQt6 import QtCore
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QGridLayout, QWidget
 
 # Subclass QMainWindow to customize application's main window
 class MainWindow(QMainWindow):
@@ -21,14 +21,41 @@ class MainWindow(QMainWindow):
         self.button.clicked.connect(self.buttonToggled)
         self.button.setChecked(self.isButtonChecked)
 
-        # Create a texbox and link it with the button
-        self.input = QLineEdit()
-        self.input.textChanged.connect(self.button.setText)
+        # Create a textbox and label for slide max length
+        self.slideMaxLengthInput = QLineEdit()
+        self.slideMaxLengthInput.setText("4")
+        self.slideMaxLengthLabel = QLabel()
+        self.slideMaxLengthLabel.setText("Max slide length (in)")
+
+        # Create a textbox and label for slide min length
+        self.slideMinLengthInput = QLineEdit()
+        self.slideMinLengthInput.setText("0")
+        self.slideMinLengthLabel = QLabel()
+        self.slideMinLengthLabel.setText("Min slide length (in)")
+
+        # Create a textbox and label for slide max speed
+        self.slideMaxSpeedInput = QLineEdit()
+        self.slideMaxSpeedInput.setText("4")
+        self.slideMaxSpeedLabel = QLabel()
+        self.slideMaxSpeedLabel.setText("Max slide speed (in/sec)")
+
+        # Create a textbox and label for slide min speed
+        self.slideMinSpeedInput = QLineEdit()
+        self.slideMinSpeedInput.setText("2")
+        self.slideMinSpeedLabel = QLabel()
+        self.slideMinSpeedLabel.setText("Min slide speed before error thrown (in/sec)")
 
         # Create the layout for the app
-        layout = QVBoxLayout()
-        layout.addWidget(self.input)
-        layout.addWidget(self.button)   
+        layout = QGridLayout()
+        layout.addWidget(self.slideMaxLengthLabel, 0, 0)
+        layout.addWidget(self.slideMaxLengthInput, 0, 1)
+        layout.addWidget(self.slideMinLengthLabel, 1, 0)
+        layout.addWidget(self.slideMinLengthInput, 1, 1)
+        layout.addWidget(self.slideMaxSpeedLabel, 2, 0)
+        layout.addWidget(self.slideMaxSpeedInput, 2, 1)
+        layout.addWidget(self.slideMinSpeedLabel, 3, 0)
+        layout.addWidget(self.slideMinSpeedInput, 3, 1)
+        layout.addWidget(self.button, 4, 1)   
 
         # Utilize the layout as a widget
         container = QWidget()
