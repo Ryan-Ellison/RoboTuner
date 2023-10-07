2cccc8716575ce02b9613c490462b5e51193ca4c
Ryan-Ellison <Ryan.Ellison477@gmail.com>
2023-10-07 15:35:43 -0400

    Created tabs easy page navigation

---
+++ desktopApplication/tabManager.py
@@ -0,0 +1,33 @@
+import sys
+from editSettingsPage import ProfileInputWindow
+from landingPage import LandingPage
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTabWidget,
+)
+
+class TabManager(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("RoboTuner")
+
+        tabs = QTabWidget()
+        tabs.setTabPosition(QTabWidget.TabPosition.North)
+        tabs.setMovable(True)
+        tabs.setDocumentMode(True)
+
+        tabs.addTab(LandingPage(), "Landing Page")
+        tabs.addTab(ProfileInputWindow(), "Edit settings")
+
+        self.setCentralWidget(tabs)
+
+app = QApplication(sys.argv)
+
+window = TabManager()
+window.show()
+
+app.exec()
