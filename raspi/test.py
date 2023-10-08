ab5044996db4686c2be47f8680fed29ed9acc54b
MuayBoy <zachderuiter@gmail.com>
2023-10-08 16:45:10 -0400

    found distances per step and made motor quieter

--- raspi/test.py
+++ raspi/test.py
@@ -9,22 +9,58 @@ import keyboard
 
 os.system("stty -echo")
 
-kit = MotorKit(i2c=board.I2C())
-stepper_style = stepper.MICROSTEP
+kit2 = MotorKit(i2c=board.I2C(), steppers_microsteps=2)
+kit4 = MotorKit(i2c=board.I2C(), steppers_microsteps=4)
+kit8 = MotorKit(i2c=board.I2C(), steppers_microsteps=8)
+kit16 = MotorKit(i2c=board.I2C(), steppers_microsteps=16)
+kits = [kit2, kit4, kit8, kit16]
+current_kit = 0
+
+stepper_style = stepper.DOUBLE
 current_step = 0
+prev_step = 0
+step_mm_conv = 0.02
+kit_mm_conv = [1, 2, 4, 8]
 
 def print_step(x):
-	print(f"current step: {current_step}")
+	print(f"current step: {current_step},	current dist: {(current_step-prev_step)*0.02/kit_mm_conv[current_kit]}mm")
+	
+def release_kits(x):
+	for i in range(4):
+		if i != x:
+			kits[i].stepper1.release()
 
-keyboard.on_release_key('w', print_step)
-keyboard.on_release_key('s', print_step)
+#keyboard.on_release_key('w', print_step)
+#keyboard.on_release_key('s', print_step)
 
 while True:
+	if keyboard.is_pressed('1'):
+		current_kit = 0
+		release_kits(0)
+		
+	if keyboard.is_pressed('2'):
+		current_kit = 1
+		release_kits(1)
+		
+	if keyboard.is_pressed('3'):
+		current_kit = 2
+		release_kits(2)
+		
+	if keyboard.is_pressed('4'):
+		current_kit = 3
+		release_kits(3)
+		
 	if keyboard.is_pressed('w'):
-		current_step = kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper_style)
+		prev_step = current_step
+		for i in range(1000):
+			current_step = kits[current_kit].stepper1.onestep(direction=stepper.FORWARD, style=stepper_style)
+		print_step(None)
 		
 	elif keyboard.is_pressed('s'):
-		current_step = kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper_style)
+		prev_step = current_step
+		for i in range(1000):
+			current_step = kits[current_kit].stepper1.onestep(direction=stepper.BACKWARD, style=stepper_style)
+		print_step(None)
 		
 	elif keyboard.is_pressed('a') and stepper_style != stepper.MICROSTEP:
 		stepper_style = stepper.MICROSTEP
@@ -38,7 +74,7 @@ while True:
 		break
 		
 	else:
-		kit.stepper1.release()
+		release_kits(-1)
 
 os.system("stty echo")
 termios.tcflush(sys.stdin, termios.TCIOFLUSH)
