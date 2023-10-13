25202e8f952604c4303070e9a71fdcad028e222c
MuayBoy <zachderuiter@gmail.com>
2023-10-13 13:14:01 -0400

    prepare test.py for demo

--- raspi/test.py
+++ raspi/test.py
@@ -6,6 +6,10 @@ import board
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 import keyboard
+import RPi.GPIO as GPIO
+
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(22, GPIO.OUT)
 
 os.system("stty -echo")
 
@@ -29,9 +33,14 @@ def release_kits(x):
 	for i in range(4):
 		if i != x:
 			kits[i].stepper1.release()
+			
+#def led_off(x):
+#	GPIO.output(22, GPIO.LOW)
+#	GPIO.cleanup()
 
 #keyboard.on_release_key('w', print_step)
 #keyboard.on_release_key('s', print_step)
+#keyboard.on_release_key('l', led_off)
 
 while True:
 	if keyboard.is_pressed('1'):
@@ -50,17 +59,22 @@ while True:
 		current_kit = 3
 		release_kits(3)
 		
+	#if keyboard.is_pressed('l'):
+		#GPIO.output(22, GPIO.HIGH)
+		
 	if keyboard.is_pressed('w'):
 		prev_step = current_step
-		for i in range(1000):
-			current_step = kits[current_kit].stepper1.onestep(direction=stepper.FORWARD, style=stepper_style)
-		print_step(None)
+		#for i in range(1000):
+		current_step = kits[current_kit].stepper1.onestep(direction=stepper.FORWARD, style=stepper_style)
+		time.sleep(0.001)
+		#print_step(None)
 		
 	elif keyboard.is_pressed('s'):
 		prev_step = current_step
-		for i in range(1000):
-			current_step = kits[current_kit].stepper1.onestep(direction=stepper.BACKWARD, style=stepper_style)
-		print_step(None)
+		#for i in range(1000):
+		current_step = kits[current_kit].stepper1.onestep(direction=stepper.BACKWARD, style=stepper_style)
+		time.sleep(0.001)
+		#print_step(None)
 		
 	elif keyboard.is_pressed('a') and stepper_style != stepper.MICROSTEP:
 		stepper_style = stepper.MICROSTEP
