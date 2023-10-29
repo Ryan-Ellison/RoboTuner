107c984a2678d856358d7ea5058863a9c98c9f1e
MuayBoy <zachderuiter@gmail.com>
2023-10-29 13:27:06 -0400

    added resistance check and home functions

--- raspi/Motor.py
+++ raspi/Motor.py
@@ -13,10 +13,28 @@ tmc.set_internal_rsense(False)
 tmc.set_acceleration(1000)
 tmc.set_max_speed(500)
 
+def stall_callback(_):
+	'''if tmc.get_stallguard_result() == 0:
+		return'''
+	print("StallGuard!	:	", tmc.get_stallguard_result())
+	tmc.stop(stop_mode=StopMode.SOFTSTOP)
+	
+def home():
+	tmc.run_to_position_steps(-100000, MovementAbsRel.RELATIVE)
+	tmc.set_current_position(0)
+	print(tmc.get_current_position())
+	
+#make steps to linear dist func
+	
+tmc.set_stallguard_callback(26, 50, stall_callback, 100)
+
 tmc.set_motor_enabled(True)
 
-success = tmc.run_to_position_steps(2000, MovementAbsRel.RELATIVE)
+home()
 time.sleep(2)
-success = tmc.run_to_position_steps(-2000, MovementAbsRel.RELATIVE)
+tmc.run_to_position_steps(10, MovementAbsRel.RELATIVE)
+tmc.run_to_position_steps(2000, MovementAbsRel.RELATIVE)
+time.sleep(2)
+tmc.run_to_position_steps(-2000, MovementAbsRel.RELATIVE)
 
 tmc.set_motor_enabled(False)
