07b41643227d4774ed2370da305f27ec98df5440
Ryan-Ellison <Ryan.Ellison477@gmail.com>
2023-11-02 21:41:54 -0400

    Updates to read a dict for future implementation

--- raspi/tuningTendencyPage.py
+++ raspi/tuningTendencyPage.py
@@ -1,9 +1,8 @@
 
 import Note
 import sys
-from profile import Profile
-from datetime import datetime
 from functools import cmp_to_key
+import Tuner
 
 from PyQt6.QtWidgets import (
     QComboBox,
@@ -18,7 +17,6 @@ from PyQt6.QtWidgets import (
     QDialogButtonBox,
     QVBoxLayout, QApplication
 )
-from PyQt6.QtGui import QIntValidator
 
 
 # Subclass QMainWindow to customize application's profile setting menu
@@ -29,12 +27,23 @@ class TuningTendencyWindow(QMainWindow):
         # Define the title of the application
         self.setWindowTitle("Tuning Information")
 
+        notes = Tuner.initialize_notes()
+        notesDict = {note.name:note.tendency for note in notes}
+
         layout = QGridLayout()
         # Create a button and a variable that tracks if the button is toggled
         note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
         octaves = [1, 2, 3, 4, 5, 6, 7, 8]
         count_i = 0
         count_j = 0
+        for note in notesDict:
+            print(note)
+            label = QLabel()
+            label.setText(" " + note + " - " + str(notesDict[note]) + " ")
+            layout.addWidget(label, count_i // 8, count_j % 8)
+            count_i += 1
+            count_j += 1
+        """
         for i in octaves:
             for j in note_names:
                 label = QLabel()
@@ -43,6 +52,7 @@ class TuningTendencyWindow(QMainWindow):
                 count_j += 1
             count_i += 1
             count_j = 0
+        """
 
         container = QWidget()
         container.setLayout(layout)
