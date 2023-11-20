bd6db40c215927dcd6cc2f1283c96453ad65c373
adamdavis <davi1506@purdue.edu>
2023-11-19 21:40:33 -0500

    Added the average overall tendency computation and the GUI for computing the tendency for a range of notes to the tuning tendency page.

--- desktopApplication/tuningTendencyPage.py
+++ desktopApplication/tuningTendencyPage.py
@@ -6,7 +6,7 @@ pyaudioPath = str(Path(__file__).parent.parent) + "/raspi"
 print("path = " + pyaudioPath)
 sys.path.insert(0, pyaudioPath)
 print("path = " + str(sys.path))
-import Tuner
+#import Tuner
 
 
 
@@ -15,7 +15,7 @@ from PyQt6.QtWidgets import (
     QLabel,
     QGridLayout,
     QWidget,
-    QApplication
+    QApplication, QTabWidget, QLineEdit, QPushButton
 )
 from PyQt6.QtGui import QIntValidator
 
@@ -31,33 +31,65 @@ class TuningTendencyWindow(QMainWindow):
         file_content = file.read()
         notesDict = json.loads(file_content)
 
-        layout = QGridLayout()
+
+        allLayout = QGridLayout()
         # Create a button and a variable that tracks if the button is toggled
         note_names = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
         octaves = [1, 2, 3, 4, 5, 6, 7, 8]
         count_i = 0
         count_j = 0
+        avgTendency = 0
         for note in notesDict:
             print(note)
             label = QLabel()
             label.setText(" " + note + " - " + str(round(notesDict[note], 3)))
-            layout.addWidget(label, count_i // 8, count_j % 8)
+            avgTendency += round(notesDict[note], 3)
+            allLayout.addWidget(label, count_i // 8, count_j % 8)
             count_i += 1
             count_j += 1
-        """
-        for i in octaves:
-            for j in note_names:
-                label = QLabel()
-                label.setText(j + str(i) + "-")
-                layout.addWidget(label, count_i, count_j)
-                count_j += 1
-            count_i += 1
-            count_j = 0
-        """
 
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+        avgTendency /= len(notesDict)
+        mainLayout = QGridLayout()
+        self.all = QWidget()
+        self.computeAverage = QWidget()
+        self.tabs = QTabWidget()
+
+        self.tabs.addTab(self.all, "Tuning Tendencies")
+        self.tabs.addTab(self.computeAverage, "Compute Averages")
+        self.setLayout(mainLayout)
+
+        self.all.layout = allLayout
+        self.all.setLayout(self.all.layout)
+
+        avgTendencyLabel = QLabel()
+        avgTendencyLabel.setText("Average Tendency = " + str(avgTendency))
+
+        averageRangeTendency = QLabel()
+        averageRangeTendency.setText("Average Tendency = ")
+
+        computeTendencyBoxLow = QLineEdit()
+        computeTendencyBoxLow.setText("From")
+        computeTendencyBoxHigh = QLineEdit()
+        computeTendencyBoxHigh.setText(("To"))
+        computeTendencyButton = QPushButton()
+        computeTendencyButton.setText("Compute")
+
+
+
+
+
+        self.computeAverage.layout = QGridLayout()
+        self.computeAverage.layout.addWidget(avgTendencyLabel)
+        self.computeAverage.layout.addWidget(computeTendencyBoxLow)
+        self.computeAverage.layout.addWidget(computeTendencyBoxHigh)
+        self.computeAverage.layout.addWidget(computeTendencyButton)
+        self.computeAverage.layout.addWidget(averageRangeTendency)
+        self.computeAverage.setLayout(self.computeAverage.layout)
+
+        mainLayout.addWidget(self.tabs)
+        self.setLayout(mainLayout)
+
+        self.setCentralWidget(self.tabs)
 
 """
 app = QApplication(sys.argv)
